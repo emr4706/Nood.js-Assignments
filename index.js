f7b98aec85e74d9d1cf9c4e1242ff1f38373bb3b
emr4706 <emr4706@gmail.com>
2019-08-29 12:33:41 +0200

    update

--- index.js
+++ index.js
@@ -2,55 +2,57 @@
 //Create a function that writes all numbers between given 2 values in a text(txt) file.
 // _Example: writetext(22,55)
 
-const fs = require('fs');
+var fs = require('fs');
 
-const writetext = (a,b) => {
+function writetext(a,b) {
+
     let myNumbers=[];
     for(let i = a; i <= b; i++){
+
         myNumbers.push(i);
-    }
-   return myNumbers;
+
+    };
+    
+    fs.writeFileSync('./Text.txt', myNumbers);
+
+}
+writetext(22,55);
+
+// //2. Read from .txt file
+// // Create a function that gets the content of given text file and prints it on the console.
+// // _Example: printText('mytextFile.txt')
+
+// // const fs = require('fs'); 
+
+function readTheFile(fileName) {
+    let fileText = fs.readFileSync(`./${fileName}`).toString();
+
+    return fileText;
 }
 
-fs.writeFile('Text.txt', writetext, (err) => {
-    if (err) throw err;
+readTheFile('Text.txt');
 
-    console.log('writetext saved!');
-});
-
-console.log(writetext(22,55));
-
-//2. Read from .txt file
-// Create a function that gets the content of given text file and prints it on the console.
-// _Example: printText('mytextFile.txt')
-
-const fs = require('fs'); 
-
-const printText = (fileName)=>{
-    fs.readFile('mytextFile.txt', 'utf8', (err, data)=> {
-        if (err) throw err;
-        console.log(data);
-    })
-};
-console.log(printText('mytextFile.txt'));
 
 // 3. Merge files
 // Create a function that gets the content of given text files and creates a new file with all the content.
+
 // _Example: mergeFiles('mytextFile.txt', 'yourTextFile.txt')
-// const fs = require('fs');
 
-const fs = require('fs');
+function mergeFiles(fileName1, fileName2) {
 
-const mergeFiles = (file1, file2) => {
+    let textOne = readTheFile(fileName1);
+    let textTwo = readTheFile(fileName2);
 
-    fs.copyFileSync(file1, file2); 
+    console.log(textOne, textTwo);
 
-};
-console.log(mergeFiles("mytextFile.txt", "yourTextFile.txt"));
+    fs.appendFileSync('./newFile.txt', textOne + textTwo);
+}
+mergeFiles('Text.txt', 'secondFile.txt');
 
-// 4. Using external module
-// Install and import moment.js(a js libary) via NPM and create a function that prints the day name of X(given number as an argument) days later on the console by using moment.js
-// _Example: getFutureDay()
+
+// // 4. Using external module
+// // Install and import moment.js(a js libary) via NPM and create a function that prints the day name of X(given number as an argument) days later on the console by using moment.js
+// // _Example: getFutureDay()
 
 var moment = require('moment');
 
